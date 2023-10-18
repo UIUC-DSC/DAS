1517839fd4a90764ff6dfac9fc6b7fe82983bf3c
Danny Silverstein <146115185+dhsilv@users.noreply.github.com>
2023-10-18 12:35:56 -0500

    Update cleaningRealtor.py

--- cleaningRealtor.py
+++ cleaningRealtor.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import mysql.connector
-
+#Get the file path of the csv we are trying to clean
 file_path = "C:\\Users\\dansi\\OneDrive\\DSC Sem 1\\president\\president_county_candidate.csv"
 df = pd.read_csv(file_path)
+
+#Create a new df from the original df where the only rows are ones that contain DEM or REP under the party column
 dem_rep = df[df['party'].isin(['DEM', 'REP'])]
 print(dem_rep)
-#cleaned_df = df[df['state'] != 'Puerto Rico']
+
+#Save this new df as a csv
 save_path = "C:\\Users\\dansi\\OneDrive\\DSC Sem 1\\president-cleaned\\df.csv"
 dem_rep.to_csv(save_path, index=False)
 print(f"Data saved to {save_path}")
